59f9b9f2aba0ffc243793dc284a6b323e0d01182
Your name <a.m.verdine@gmail.com>
2020-10-07 01:25:19 -0400

    Complete no_dups.py

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,16 @@
 def no_dups(s):
     # Your code here
+    used_words = set()
+    result = ''
+    words = s.split()
+    for word in words:
+        if word not in used_words:
+            result += word + ' '
+            used_words.add(word)
 
+    if result.endswith(' '):
+        result = result[:-1]
+    return result
 
 
 if __name__ == "__main__":
